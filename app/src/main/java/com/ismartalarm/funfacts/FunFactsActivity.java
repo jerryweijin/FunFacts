4fc27fa176dfbc447481d8cc71bcbddc4abb4154
Jerry Yu <jerryweijin@hotmail.com>
2017-05-09 23:53:32 -0700

    Initial commit

---
+++ app/src/main/java/com/ismartalarm/funfacts/FunFactsActivity.java
@@ -0,0 +1,29 @@
+package com.ismartalarm.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FunFactsActivity extends AppCompatActivity {
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+    }
+}
